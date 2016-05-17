b96d3dec6cb7ab5737027bc4d3c5efc6ce620a9f
Kane <fuliang1025@outlook.com>
2016-05-17 11:09:22 +0800

    check_eror.py works

--- check_error.py
+++ check_error.py
@@ -18,15 +18,15 @@ def prob(alpha_matrix,bin_num,shift_num):
     prob_count = np.zeros((bin_num,bin_num))
     prob_matrix = np.zeros((bin_num,bin_num))
     rank_series = alpha_matrix.apply(lambda x:pd.qcut(x,bin_num))
-    rank_matrix = np.array([rank_ser[i].labels for i in xrange(len(rank_series))])
+    rank_matrix = np.array([rank_series[i].labels for i in xrange(len(rank_series))])
     
     for ii in range(len(rank_matrix)-shift_num):
         for jj in range(len(rank_matrix[0])):
             if(rank_matrix[ii][jj]!=-1) and (rank_matrix[ii+shift_num][jj]!=-1):
                 prob_count[rank_matrix[ii][jj],rank_matrix[ii+shift_num][jj]]+=1
 
-    for ii in range(prob_matrix):
-        for jj in range(prob_matrix[0]):
+    for ii in range(len(prob_matrix)):
+        for jj in range(len(prob_matrix[0])):
             prob_matrix[ii][jj]=prob_count[ii][jj]/(sum(prob_count[ii])+0.0)
     return prob_matrix
 
@@ -42,7 +42,8 @@ for ii in range(len(ind)):
 for (ii,name) in enumerate(fund_names):
     alpha=np.loadtxt(('all_output/'+'fund_'+name+'_alpha_median.txt'))
     all_alpha.ix[0:(len(alpha)-1),ii]=alpha
-
+print all_alpha
+all_alpha = all_alpha[:239]
 fund_time = pd.date_range(start = '1996-01-31',end = '2015-11-30',freq='M')
 all_alpha.columns = fund_names
 all_alpha.index = fund_time
@@ -51,9 +52,9 @@ print 'temp'
 print alphas
 print alphas.shape
 cmap = plt.cm.cool
-shift_month = 12 
+shift_month = 36 
 prob_matrix = prob(alphas,5,shift_month)
 plt.figure()
-plt.imshow(prob,cmap = camp)
+plt.imshow(prob_matrix,cmap = cmap)
 plt.colorbar()
 plt.show()
